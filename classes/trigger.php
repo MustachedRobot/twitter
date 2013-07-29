115bf9385d1e343896df3301831433f24adda970
Jérémie Pottier <jeremie.pottier@gmail.com>
2013-07-29 13:48:29 +0200

    use the right getConfig options

--- classes/trigger.php
+++ classes/trigger.php
@@ -1,57 +1,49 @@
 <?php
 
 namespace Twitter;
+use Mustached\Plugin;
 
 class Trigger
 {
 
-	/**
-	 * This action is triggered after a user checks in. 
-	 * If the user has checked the checkbox "publish on twitter" on the checkin form, a tweet is sent on behalf of the coworking space twitter account
-	 * 
-	 * @param  Array $options Options 
-	 */
-	public function postCheckin($options)
-	{
-		\Lang::load('twitter::twitter.yml', 'twitter');
-		
-		$fieldset = $options['fieldset'];
-		
-		if ($fieldset->validation()->run() == true)
-        {
-			$fields = $fieldset->validated();
-			
-			if($fields['twitter'])
-			{
-				include('vendor/twitter-php/library/twitter.class.php');
+    /**
+     * This action is triggered after a user checks in. 
+     * If the user has checked the checkbox "publish on twitter" on the checkin form, a tweet is sent on behalf of the coworking space twitter account
+     * 
+     * @param Array $options Options 
+     * 
+     * @return Mixed True on Success
+     */
+    public function postCheckin($options)
+    {
+        \Lang::load('twitter::twitter.yml', 'twitter');
+        
+        $fieldset = $options['fieldset'];
+        
+        if ($fieldset->validation()->run() == true) {
+            $fields = $fieldset->validated();
+            if ($fields['twitter']) {
 
-				$um = new \User\Manager;
-				$user = $um->get_user_from_email($fields['email']);
+                include 'vendor/twitter-php/library/twitter.class.php';
 
-				$c = new \Checkin\Manager;
-				$reason = $c->get_reason($fields['reason']);
+                $um = new \User\Manager;
+                $user = $um->get_user_from_email($fields['email']);
 
-				$twitter_name = empty($user['twitter']) ? '' : '(@'.$user['twitter'].')';
+                $c = new \Checkin\Manager;
+                $reason = $c->get_reason($fields['reason']);
 
-				$config = \Config::load('twitter::twitter', 'twitter');
+                $twitter_name = empty($user['twitter']) ? '' : '(@'.$user['twitter'].')';
 
-				try 
-				{
-					$twitter = new \Twitter(\Config::get('twitter.consumerKey.value'), \Config::get('twitter.consumerSecret.value'), \Config::get('twitter.accessToken.value'), \Config::get('twitter.accessTokenSecret.value'));
-					$twitter->send(__('twitter.tweet', array('firstname' => $user['firstname'], 'lastname' => $user['lastname'], 'pseudo' => $twitter_name, 'reason' => $reason)));	
-				}
-				catch(Exception $e)
-				{
-									
-				}
-
-				return true;
-
-			}
-			else 
-			{
-				//echo 'No tweet';
-			}
-		}		
-	}
+                try {
+                    $twitter = new \Twitter(Plugin::getConfig('twitter', 'consumerKey.value'), Plugin::getConfig('twitter', 'consumerSecret.value'), Plugin::getConfig('twitter', 'accessToken.value'), Plugin::getConfig('twitter', 'accessTokenSecret.value'));
+                    $twitter->send(__('twitter.tweet', array('firstname' => $user['firstname'], 'lastname' => $user['lastname'], 'pseudo' => $twitter_name, 'reason' => $reason))); 
+                } catch(Exception $e) {
+                                    
+                }
+                return true;
+            } else {
+                //echo 'No tweet';
+            }
+        }       
+    }
 }
